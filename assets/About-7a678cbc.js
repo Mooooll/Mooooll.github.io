baded4fc8cfc943d063104158cb2f6869113eff7
Mooooll <maoll18@qq.com>
2024-03-23 19:32:12 +0800

    update

---
+++ assets/About-7a678cbc.js
@@ -0,0 +1,1 @@
+import{b as t,c as e,d as s}from"./index-82635baf.js";const o={class:"about flex justify-center items-start"},a=s("span",{class:"text-5xl"},"INFP",-1),c=[a],l={__name:"About",setup(_){return(n,r)=>(t(),e("div",o,c))}};export{l as default};
