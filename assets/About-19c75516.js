e65cdaa2cc855a6d2f628837b8812c66b9586ac8
Mooooll <maoll18@qq.com>
2024-03-25 20:48:53 +0800

    update

--- assets/About-19c75516.js
+++
@@ -1,1 +0,0 @@
-import{b as t,c as e,d as s}from"./index-2f1ebdc9.js";const o={class:"about flex justify-center items-start"},a=s("span",{class:"text-5xl"},"INFP",-1),c=[a],l={__name:"About",setup(_){return(n,r)=>(t(),e("div",o,c))}};export{l as default};
