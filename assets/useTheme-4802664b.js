61851a9abc2ccb3a9607b72449bcc2b27228bae1
Mooooll <986490854@qq.com>
2024-12-10 22:19:06 +0800

    update

---
+++ assets/useTheme-4802664b.js
@@ -0,0 +1,1 @@
+import{r as n,D as r,b as u,c as o,o as l}from"./index-639a23ae.js";function m(){let e=n(document.documentElement.classList[0]??"light"),t=r(()=>"bg-"+e.value);u(()=>{o.on("darkMode",a)});const a=s=>{s?e.value="dark":e.value="light"};return l(()=>{o.off("darkMode")}),t}export{m as u};
